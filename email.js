075af1a5e7c5a9073d03d9a8d64318e078dc580b
Jyotsna <jyotsnasehgal24@gmail.com>
2024-09-27 07:02:08 -0400

    changes

--- email.js
+++ email.js
@@ -2,15 +2,13 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(
   "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"
 );
-const msg = {
-  to: "jyotsnasehgal24@gmail.com", // Change to your recipient
-  from: "jyotsnasehgal24@gmail.com", // Change to your verified sender
-  subject: "Form Submitted Successfully",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-};
+
 const createmsg = (data) => {
-  return `US Land Funders
+  return {
+    to: "jyotsnasehgal24@gmail.com", // Change to your recipient
+    from: "jyotsnasehgal24@gmail.com", // Change to your verified sender
+    subject: "Form Submitted Successfully",
+    text: `US Land Funders
 Do you have a signed purchase agreement? : Yes
 First Name: Charlie 
 Last Name: Steele
@@ -28,10 +26,9 @@ Closing Date:${data.ClosingDate}
 If you've talked to a Real Estate Agent…
 Recently Sold Comps:${data.SoldComps}
 For Sale comps:${data.SaleComps}
-Any other thoughts or issues?:${data.AdditionalInfo}
-
-    
-    `;
+Any other thoughts or issues?:${data.AdditionalInfo} `,
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
 };
 const sendEmail = async (data) => {
   msg = createmsg(data);
